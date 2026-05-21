18fb528adeac472b5c5c1ac1eff6bbf16f57d14e
sunjaewow <chltjswo789@naver.com>
2026-05-21 15:38:24 +0900

    first settings

---
+++ src/main/java/com/petcommunity/petcommunityworker/PetCommunityWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.petcommunity.petcommunityworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetCommunityWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetCommunityWorkerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/petcommunity/petcommunityworker/PetCommunityWorkerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.petcommunity.petcommunityworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetCommunityWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
